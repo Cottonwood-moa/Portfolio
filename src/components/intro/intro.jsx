bea23f312691063682252c478758396c60822e2c
Cottonwood <blog_geon@naver.com>
2022-01-18 01:40:29 +0900

    add About UI

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,13 +1,16 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React, { useEffect, useRef, useState } from "react";
 import Nav from "../nav/nav";
 import styles from "./intro.module.scss";
 import Loading from "../loading/loading";
-import Tag from "../common/tag/tag";
 import { Container } from "react-bootstrap";
+import ReactTooltip from "react-tooltip";
+import { useNavigate } from "react-router-dom";
 
 const Intro = (props) => {
   const now = new Date();
   const year = now.getFullYear();
+  const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
   return (
     <>
@@ -26,23 +29,87 @@ const Intro = (props) => {
                 <div className={styles.textElement6}>M</div>
                 <div className={styles.textElement7}>E</div>
               </div>
-              <div className={styles.image}></div>
+
+              <div className={styles.follows}>
+                <ReactTooltip id="codepenTip" type="error" effect="float">
+                  Codepen
+                </ReactTooltip>
+                <a
+                  href="https://codepen.io/Cottonwood"
+                  data-tip
+                  data-for="codepenTip"
+                  className={`${styles.follow} ${styles.codepen}`}
+                ></a>
+                <ReactTooltip id="githubTip" type="error" effect="float">
+                  Github
+                </ReactTooltip>
+                <a
+                  href="https://github.com/Cottonwood-moa"
+                  data-tip
+                  data-for="githubTip"
+                  className={`${styles.follow} ${styles.github}`}
+                ></a>
+                <div className={styles.image}></div>
+                <ReactTooltip id="blogTip" type="error" effect="float">
+                  Blog
+                </ReactTooltip>
+                <a
+                  href="https://cottonwood-moa.tistory.com/"
+                  data-tip
+                  data-for="blogTip"
+                  className={`${styles.follow} ${styles.techBlog}`}
+                ></a>
+                <ReactTooltip id="phoneTip" type="error" effect="float">
+                  +82-10-2831-6735
+                </ReactTooltip>
+                <div
+                  data-tip
+                  data-for="phoneTip"
+                  className={`${styles.follow} ${styles.phone}`}
+                ></div>
+              </div>
               <div className={styles.info}>
+                <h1>Hello, my name is Geonwoo Park.</h1>
                 <div>
-                  <h1>Hello, my name is Geonwoo Park.</h1>
-                  <div>
-                    I am front-end web developer and UI/UX javascript
-                    specialist.
-                  </div>
-                  <div>
-                    Check out my articles, React and React Native UI components
-                    at the <span className={styles.link}>code laboratory</span>.
-                  </div>
-                  <div>
-                    Feel free to take a look at my latest projects on the web
-                    <span className={styles.link}>portfolio page</span>.
-                  </div>
-                  <div>Copyrightⓒ{year} by Cottonwood(Geonwoo Park).</div>
+                  I am front-end web developer and UI/UX javascript specialist.
+                </div>
+                <div>
+                  Check out my articles, React and React Native UI components at
+                  the
+                  <ReactTooltip id="labsTip" type="error" effect="float">
+                    Go to Code laboratory
+                  </ReactTooltip>
+                  <span
+                    className={styles.link}
+                    data-tip
+                    data-for="labsTip"
+                    onClick={() => {
+                      navigate("/labs");
+                    }}
+                  >
+                    code laboratory
+                  </span>
+                  .
+                </div>
+                <div>
+                  Feel free to take a look at my latest projects on the web
+                  <ReactTooltip id="portfolioTip" type="error" effect="float">
+                    Go to Portfolio
+                  </ReactTooltip>
+                  <span
+                    className={styles.link}
+                    data-tip
+                    data-for="portfolioTip"
+                    onClick={() => {
+                      navigate("/portfolio");
+                    }}
+                  >
+                    portfolio page
+                  </span>
+                  .
+                </div>
+                <div className={styles.copyright}>
+                  Copyrightⓒ{year} by Cottonwood(Geonwoo Park).
                 </div>
               </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import App from "./app";
 
 ReactDOM.render(
